845461e0b957f78bb1b9e83bc9eac324e66ac1b1
mervenuur <mervenurportakal41@hotmail.com>
2021-01-24 19:17:49 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç İçin Temel Kurs", "Java" ,"python" };
+
+            for (int i = 0; i < kurslar.Length; i++) //i++ , i=i+2 , i+=2 //length eleman
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("for bitti");
+            foreach (string kurs in kurslar) //dizileri tek tek dolaşmaya yarar 
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ odev/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Kitap kitap1 = new Kitap();
+            kitap1.KitapAdi = "Şeker Portakalı";
+            kitap1.KitapYazari = "Jose Mauro de Vasconcelos";
+            kitap1.KitapNo = 1;
+
+            Kitap kitap2 = new Kitap();
+            kitap2.KitapAdi = "Açlık Oyunları";
+            kitap2.KitapYazari = "Suzanne Collins";
+            kitap2.KitapNo = 2;
+
+            Kitap kitap3 = new Kitap();
+            kitap3.KitapAdi = "Tansık";
+            kitap3.KitapYazari = "Günay Uslu";
+            kitap3.KitapNo = 3;
+
+            Kitap[] kitaplar = new Kitap[] { kitap1, kitap2, kitap3 };
+
+            foreach (var kitap in kitaplar)
+            {
+                Console.WriteLine(kitap.KitapAdi + " : " + kitap.KitapYazari);
+            }
+
+            Console.WriteLine("Foreach döngüsü burada bitii");
+
+            for (int i = 0; i < kitaplar.Length; i = i + 1)
+            {
+                Console.WriteLine(kitaplar[i].KitapAdi + " : " + kitaplar[i].KitapYazari + " : " + kitaplar[i].KitapNo);
+            }
+
+            Console.WriteLine("For döngüsü burada bitti");
+        }
+
+            class Kitap
+        {
+            public string KitapAdi { get; set; }
+            public string KitapYazari { get; set; }
+            public int KitapNo { get; set; }
+        }
+
+        }
+    }
+
